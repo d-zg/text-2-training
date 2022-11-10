44bfe8cba5618deb4a14bbaa254d20e76f582f71
warringloser <67976455+warringloser@users.noreply.github.com>
2022-11-10 10:33:34 -0600

    test

--- training/train_methods.py
+++ training/train_methods.py
@@ -58,8 +58,9 @@ def get_class_distribution(dataloader):
     train_classes = []
     for step, (x,y) in enumerate(dataloader):
         y.detach().tolist()
+        print(y)
         train_classes += y
-    print(Counter(train_classes))
+    # print(Counter(train_classes))
 
 
 
